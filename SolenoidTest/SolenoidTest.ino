7d26f9300a2e6437a1b861036c6e068e43351909
gorplex <gorplexthefallen@gmail.com>
2018-10-16 14:35:18 -0700

    changed pins

--- SolenoidTest/SolenoidTest.ino
+++ SolenoidTest/SolenoidTest.ino
@@ -1,19 +1,18 @@
 //BTNS
-#define BTN1 8
-#define BTN2 10
-#define TMP 9
+#define BTN1 10
+#define LED1 12
+#define BTN2 8
 
 //SOL
-#define SOL1 6
-#define SOL2 7
+#define SOL1 11
+#define SOL2 9
 
 void setup() {
   //BTNS
   pinMode(BTN1, INPUT_PULLUP);
+  pinMode(LED1, OUTPUT);
   pinMode(BTN2, INPUT_PULLUP);
-  pinMode(TMP, OUTPUT);
-  digitalWrite(TMP, LOW);
-
+  
   //SOL
   pinMode(SOL1, OUTPUT);
   pinMode(SOL2, OUTPUT);
@@ -22,8 +21,10 @@ void setup() {
 void loop() {
   if(LOW == digitalRead(BTN1)){
     digitalWrite(SOL1, HIGH);
+    digitalWrite(LED1, LOW);
   }else{
     digitalWrite(SOL1, LOW);
+    digitalWrite(LED1, HIGH);
   }
   if(LOW == digitalRead(BTN2)){
     digitalWrite(SOL2, HIGH);
